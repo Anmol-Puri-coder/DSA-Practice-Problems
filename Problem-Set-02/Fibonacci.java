ef96a47f90c2355db4d5370c69b49d95f16d36ef
Anmol Puri <anmolpuri954@gmail.com>
2024-08-09 13:18:27 +0530

    Add files via upload

---
+++ Problem-Set-02/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.dsa;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int n = in.nextInt();
+        fib(n);
+    }
+    static void fib(int n){
+    int num1 = 0;
+    int num2 = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i +" = "+ num1+" || ");
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+        }
+
+    }
+}
+
